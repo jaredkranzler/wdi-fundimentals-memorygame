0aadfdc7108daef45ce11b748b3bf562bb594dac
JK <JK@JKs-MacBook-Pro.local>
2018-06-07 18:15:15 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -25,14 +25,28 @@ var cardsInPlay = [];
 var checkForMatch = function () {
 	cardsInPlay[0] === cardsInPlay[1] ? alert("You found a match!"): alert("Sorry, try again.");
 };
-var flipCard = function (cardId) {
+
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-	cardsInPlay.push(cards.rank);
-if (cardsInPlay.length === 2) {
-}
-checkForMatch ();
+	this.setAttribute('src', cards[cardId].cardImage);
+		if (cardsInPlay.length === 2) {
+			checkForMatch();
+			cardsInPlay = [2];
+		}
 };
-	flipCard (0);
-	flipCard (2);
+
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
